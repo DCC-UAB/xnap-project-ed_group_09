507d11b9bfd00ad02c459e8a0566b0be5e8533d2
wachicalamelo <1604373@uab.cat>
2023-05-23 09:35:25 +0000

    creació py main i lectura dataset

---
+++ lectura_dataset.py
@@ -0,0 +1,90 @@
+from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from imgaug import augmenters as iaa
+from pathlib import Path
+import cv2
+import pandas as pd
+from PIL import Image
+
+class ImgAugTransform:
+    def __init__(self):
+        self.aug = iaa.Sequential([
+            iaa.OneOf([
+                iaa.Sometimes(0.25, iaa.AdditiveGaussianNoise(scale=0.1 * 255)),
+                iaa.Sometimes(0.25, iaa.GaussianBlur(sigma=(0, 3.0)))
+                ]),
+            iaa.Affine(
+                rotate=(-20, 20), mode="edge",
+                scale={"x": (0.95, 1.05), "y": (0.95, 1.05)},
+                translate_percent={"x": (-0.05, 0.05), "y": (-0.05, 0.05)}
+            ),
+            iaa.AddToHueAndSaturation(value=(-10, 10), per_channel=True),
+            iaa.GammaContrast((0.3, 2)),
+            iaa.Fliplr(0.5),
+        ])
+
+    def __call__(self, img):
+        img = np.array(img)
+        img = self.aug.augment_image(img)
+        return img
+
+class FaceDataset(Dataset):
+    def __init__(self, data_dir, data_type, img_size=224, augment=None,transf=None):
+        assert(data_type in ("train", "valid", "test"))
+        pathcsv = Path(data_dir).joinpath(f"gt_avg_{data_type}.csv")
+        img_dir = Path(data_dir).joinpath(data_type)
+        self.img_size = img_size
+        self.augment = augment
+
+        if augment==0:
+          self.transform = lambda i: i #no modifica la imatge
+        elif augment==1:
+          self.transform = ImgAugTransform() 
+          #aplica una serie de transformacions: (afegeix soroll, desenfoca, rotació..)
+          #ajuda al model a generalitzar, a regularitzar i que no es centri en patrons especifics.
+        else:
+          self.transform = transf
+            
+        self.x = []
+        self.y = []
+        df = pd.read_csv(str(pathcsv))
+        #ignore_path = Path(__file__).resolve().parent.joinpath("ignore_list.csv")
+        ignore_img_names = list(pd.read_csv('./AppaRealAge/appa-real-release/ignore_list.csv')['img_name'].values)
+
+        for _, row in df.iterrows():
+            img_name = row["file_name"]
+
+            if img_name in ignore_img_names:
+                continue
+
+            img_path = img_dir.joinpath(img_name + "_face.jpg")
+            assert(img_path.is_file())
+            self.x.append(str(img_path))
+            self.y.append(row["real_age"])
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+        img_path = self.x[idx]
+        age = self.y[idx]
+
+        if self.augment!=0 and self.augment!=1:
+          img = Image.open(img_path)
+          img=self.transform(img)
+          return img,age
+        else:
+          img = cv2.imread(str(img_path), 1)
+          img = cv2.resize(img, (self.img_size, self.img_size))
+          img = self.transform(img).astype(np.float32)
+          return torch.from_numpy(np.transpose(img, (2, 0, 1))),age
+        #fem np.transpose perque torch espera que les imatges siguin (canal, altura, amplada)
+
+def mostrar_imagen(dataset, indice):
+    ruta_imagen = dataset.x[indice]
+    print(ruta_imagen)
+    imagen = mpimg.imread(ruta_imagen)
+    plt.imshow(imagen)
+    plt.axis('off')
+    plt.show()
---
+++ main_project.py
@@ -0,0 +1,30 @@
+from __future__ import print_function 
+from __future__ import division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+from tqdm import tqdm
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torch.backends.cudnn as cudnn
+from torch.optim.lr_scheduler import StepLR
+from collections import OrderedDict
+
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+print("PyTorch Version: ",torch.__version__)
+print("Torchvision Version: ",torchvision.__version__)
+
+%matplotlib inline
+
+
+# Detect if we have a GPU available
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+import wandb
+from lectura_dataset import FaceDataset
