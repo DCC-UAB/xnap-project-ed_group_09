5ea2a8741ac751dca2c45b80c442e598fa46bf40
wachicalamelo <1604373@uab.cat>
2023-05-25 12:08:38 +0200

    main,model i train cacd

---
+++ CACD/model_cacd.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+from torchvision import models
+#import pretrainedmodels
+#import pretrainedmodels.utils
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+            
+def get_model(tipus=None):
+    if tipus=='finetunning':
+        model = models.resnet34(weights=True)
+        num_features = model.fc.in_features
+        model.fc = nn.Linear(in_features=num_features,out_features=1)
+        return model
+    else:
+        model = models.resnet34(weights=True) # Notice we are now loading the weights of a ResNet model trained on ImageNet
+        set_parameter_requires_grad(model,True)
+        num_features = model.fc.in_features
+        model.fc = nn.Linear(in_features=num_features,out_features=1)
+        return model
