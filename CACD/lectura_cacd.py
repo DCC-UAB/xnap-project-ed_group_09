706fb7dc672c1ce4330834dd3ff1f3a292788504
wachicalamelo <1604373@uab.cat>
2023-05-26 16:09:49 +0200

    lecturacacd

--- CACD/lectura_cacd.py
+++ CACD/lectura_cacd.py
@@ -8,6 +8,8 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from PIL import Image
+import dlib
+import np
 
 
 class CACDDataset(Dataset):
@@ -25,22 +27,67 @@ class CACDDataset(Dataset):
         self.transform = transform
 
     def __getitem__(self, index):
-        try:
-            img_path = os.path.join(self.img_dir, self.img_names[index])
-            if os.path.exists(img_path):  # Verificar si la imagen existe en el directorio
-                img = Image.open(img_path)
+        detector = dlib.get_frontal_face_detector()
+        img_path = os.path.join(self.img_dir, self.img_names[index])
+        img = Image.open(img_path)
+        detected = detector(img, 1)
 
-                if self.transform is not None:
-                    img = self.transform(img)
+        if len(detected) == 1:  # skip if there are 0 or more than 1 face
+            for idx, face in enumerate(detected):
+                width = face.right() - face.left()
+                height = face.bottom() - face.top()
+                tol = 15
+                up_down = 5
+                diff = height-width
 
-                label = self.y[index]
+                if(diff > 0):
+                    if not diff % 2:  # symmetric
+                        tmp = img[(face.top()-tol-up_down):(face.bottom()+tol-up_down),
+                                (face.left()-tol-int(diff/2)):(face.right()+tol+int(diff/2)),
+                                :]
+                    else:
+                        tmp = img[(face.top()-tol-up_down):(face.bottom()+tol-up_down),
+                                (face.left()-tol-int((diff-1)/2)):(face.right()+tol+int((diff+1)/2)),
+                                :]
+                if(diff <= 0):
+                    if not diff % 2:  # symmetric
+                        tmp = img[(face.top()-tol-int(diff/2)-up_down):(face.bottom()+tol+int(diff/2)-up_down),
+                                (face.left()-tol):(face.right()+tol),
+                                :]
+                    else:
+                        tmp = img[(face.top()-tol-int((diff-1)/2)-up_down):(face.bottom()+tol+int((diff+1)/2)-up_down),
+                                (face.left()-tol):(face.right()+tol),
+                                :]
 
-                return img, label
-            else:
-            # Si la imagen no existe, devuelve una tupla vacía
-                return None, None
-        except:
-            pass
+                try:
+                    img = np.array(Image.fromarray(np.uint8(tmp)).resize((120, 120), Image.ANTIALIAS))
+                except ValueError:
+                    if self.transform is not None:
+                        img = self.transform(img)
+
+                    label = self.y[index]
+
+                    return img, label
+            
+        if self.transform is not None:
+            img = self.transform(img)
+
+        label = self.y[index]
+
+        return img, label
+
+
+
+
+
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        label = self.y[index]
+
+        return img, label
+            
 
     def __len__(self):
         return self.y.shape[0]
