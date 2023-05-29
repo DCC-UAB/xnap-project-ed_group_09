5f678a30aab19e96db92e9bb5df16bb7c6d2d682
wachicalamelo <1604373@uab.cat>
2023-05-29 09:25:32 +0000

    changes model

--- Appa_real/model_appa_real.py
+++ Appa_real/model_appa_real.py
@@ -2,19 +2,18 @@ import torch.nn as nn
 from torchvision import datasets, models
 
 def set_parameter_requires_grad(model, feature_extracting, num_layers):
-     if feature_extracting:
+    """if feature_extracting:
         total_layers = len(list(model.modules()))   # Obtener el número total de capas en el modelo
         print(total_layers)
         for idx, param in enumerate(model.parameters()):
             if idx < total_layers - num_layers:  # Congelar los pesos de las capas anteriores a las últimas 4
                 param.requires_grad = False
             else:
-                param.requires_grad = True
+                param.requires_grad = True"""
     
-    
-#if feature_extracting:
- #       for param in model.parameters():
-  #          param.requires_grad = False
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
             
 def get_model(tipus=None):
     if tipus=='finetunning':
