cc754de514d18c7f826e78b99c3f863c2b27c944
wachicalamelo <1604373@uab.cat>
2023-05-30 08:18:41 +0000

    changes

--- Appa_real/main_appa_real.py
+++ Appa_real/main_appa_real.py
@@ -92,7 +92,7 @@ model = get_model('fe')
 model = model.to(device)
 
 name_project='AppaReal-First-Executions'
-name_run='mix_aug_0.001_32_L1'
+name_run='mix8_aug_0.001_32_L1'
 
 # Setup the loss fxn
 #criterion = nn.MSELoss()
