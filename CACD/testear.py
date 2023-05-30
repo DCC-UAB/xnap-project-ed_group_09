570cd41dd6509783f202f0191253d5c902d9ebeb
wachicalamelo <1604373@uab.cat>
2023-05-30 17:22:51 +0200

    changes test

--- CACD/testear.py
+++ CACD/testear.py
@@ -1,5 +1,7 @@
 from model_cacd import *
 import torch
+from PIL import Image
+from torchvision import datasets, models, transforms
 
 model=get_model('finetunning')
 
@@ -9,3 +11,22 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 model = model.to(device)
 model.eval()
+
+image_path = './foto.jpg'
+image = Image.open(image_path)
+
+input_tensor = transforms.ToTensor()(image)
+# Agregar una dimensión extra para representar el lote (batch)
+input_batch = input_tensor.unsqueeze(0)
+
+# Mover el tensor de entrada a la GPU si está disponible
+input_batch = input_batch.to(device)
+
+# Realizar la predicción
+with torch.no_grad():
+    output = model(input_batch)
+
+# Obtener la salida de regresión
+predicted_value = output.item()
+
+print(f'Predicción: {predicted_value}')
